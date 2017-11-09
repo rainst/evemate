e455bed6d56faf20eb2d76d7e9b89ec46db44cad
Renato <rstretti@gmail.com>
2017-11-09 22:00:40 +1100

    added kills and jumps to system

--- src/app/evesystems.service.ts
+++ src/app/evesystems.service.ts
@@ -1,30 +1,48 @@
 import { Injectable } from '@angular/core';
 import { EveAPIService } from './eveapi.service';
 import { BaseEveModel } from './eve.class';
-import { EveNamesService, NameModel } from './evenames.service';
 
 export class System extends BaseEveModel {
   constellation_id: number;
   name: string;
-  planets: {planet_id:NameModel, moons?:NameModel[]}[];
+  planets: {planet_id:number, moons?:number[]}[];
   position: {x:number, y:number, z:number};
   security_class: string;
   security_status: number;
   star_id: number;
-  stargates: NameModel[];
-  stations: NameModel[];
+  stargates: number[];
+  stations: number[];
   system_id: number;
+}
+
+export class SystemKills extends BaseEveModel {
+  system_id: number;
+  ship_kills: number;
+  npc_kills: number;
+  pod_kills: number;
+}
+
+export class SystemJumps extends BaseEveModel {
+  system_id: number;
+  ship_jumps: number;
 }
 
 @Injectable()
 
 export class EveSystemsService {
   private systems: Map<number, System> = new Map();
+  private systemKills: Map<number, SystemKills> = new Map();
+  private systemJumps: Map<number, SystemJumps> = new Map();
+
   private APISystemInfo = 'universe/systems/{SystemID}/';
+  private APISystemKills = 'universe/system_kills/';
+  private APISystemJumps = 'universe/system_jumps/';
   
+  private killsPromise: Promise<any>;
+  private jumpsPromise: Promise<any>;
+
   constructor(
-    private api: EveAPIService,
-    private names: EveNamesService
+    private api: EveAPIService
   ) {}
 
   get(systemID: number): Promise<System> {
@@ -32,21 +50,10 @@ export class EveSystemsService {
       if (this.systems.has(systemID))
         resolve(this.systems.get(systemID));
       else
-        this.api.get(this.APISystemInfo.replace('{SystemID}', systemID.toString())).then(res => {
-          // var rawSystem = res.json();
-          // var names = [];
-
-          // rawSystem.stations && (names = names.concat(rawSystem.stations));
-          // names = names.concat(rawSystem.constellation_id);
-          
+        this.api.get(this.APISystemInfo.replace('{SystemID}', systemID.toString())).then(res => {          
           var system = new System(res.json());
           this.systems.set(systemID, system);
           resolve(system);
-
-          // this.names.getNames(names).then(names => {
-          //   rawSystem.constellation = names.find(name => {return name.id === rawSystem.constellation_id});
-          //   rawSystem.stations && (rawSystem.stations = names.filter(name => {return rawSystem.stations.includes(name.id)}));
-          // });
         });
     });
   }
@@ -58,6 +65,60 @@ export class EveSystemsService {
       systemsID.forEach(systemID => promises.push(this.get(systemID)));
 
       Promise.all(promises).then(results => resolve(results));
+    });
+  }
+
+  getKills(systemID: number): Promise<SystemKills> {
+    return new Promise(resolve => {
+      if (!this.systemKills.size)
+        this.loadKills(systemID).then(kill => resolve(kill));
+      else
+        resolve(this.systemKills.get(systemID));
+    });
+  }
+
+  loadKills(systemID?: number): Promise<SystemKills | null> {
+    return new Promise(resolve => {
+      if (! this.killsPromise)
+        this.killsPromise = this.api.get(this.APISystemKills);
+
+      this.killsPromise.then(res => {
+        this.systemKills.clear();
+        var systemKills:any[] = res.json();
+
+        systemKills.forEach(item => {
+          var systemKill = new SystemKills(item);
+          this.systemKills.set(systemKill.system_id, systemKill);
+        });
+        resolve(this.systemKills.get(systemID));
+      });
+    });
+  }
+
+  getJumps(systemID: number): Promise<SystemJumps> {
+    return new Promise(resolve => {
+      if (!this.systemJumps.size)
+        this.loadJumps(systemID).then(jump => resolve(jump));
+      else
+        resolve(this.systemJumps.get(systemID));
+    });
+  }
+
+  loadJumps(systemID?: number): Promise<SystemJumps | null> {
+    return new Promise(resolve => {
+      if (! this.jumpsPromise)
+        this.jumpsPromise = this.api.get(this.APISystemJumps);
+
+      this.jumpsPromise.then(res => {
+        this.systemJumps.clear();
+        var systemJumps:any[] = res.json();
+
+        systemJumps.forEach(item => {
+          var systemJump = new SystemJumps(item);
+          this.systemJumps.set(systemJump.system_id, systemJump);
+        });
+        resolve(this.systemJumps.get(systemID));
+      });
     });
   }
 }
